78e17580afe2a5f125b6dfac568d9f7bef03c2cf
shaheenshehi <shaheen.shahee11@gmail.com>
2024-09-24 22:30:40 +0530

    projects

---
+++ weather/main.js
@@ -0,0 +1,7 @@
+const cityInput = document.querySelector('.city-input')
+const searchBtn = document.querySelector('.search-btn')
+
+searchBtn.addEventListener('click', () => {
+    console.log(cityInput.value)
+    
+})
